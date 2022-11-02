269390082a38986dec57c4ac28a5324372c42c4d
MJB2012 <matheusjbarbosa23@gmail.com>
2022-11-02 14:03:29 -0300

    TaskTimer

--- src/Reducers/cycles/reducer.ts
+++ src/Reducers/cycles/reducer.ts
@@ -1,3 +1,5 @@
+import { produce } from 'immer'
+
 import { ActionTypes } from './actions'
 
 export interface Cycle {
@@ -17,11 +19,15 @@ interface CyclesState {
 export function cyclesReducers(state: CyclesState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
-      return {
+      /* return {
         ...state,
         cycles: [...state.cycles, action.payload.newCycle],
         activeCycleId: action.payload.newCycle.id,
-      }
+      } */
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle)
+        draft.activeCycleId = action.payload.newCycle.id
+      })
     case ActionTypes.INTERRUPT_CURRENT_CYCLE:
       return {
         ...state,
